6bab247920e741b6b59b4c3e9fa731b8a9d15987
sillybowen <bz465@nyu.edu>
2017-08-17 08:08:37 +0000

    fix

---
+++ alexnet/alexnet_v1.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''
+Author: sunlei
+Mail: sunlei@conew.com
+Last modified: 2017-07-27 13:14
+'''
+
+from numpy import *
+import os
+import tensorflow as tf
+
+weights_initializer = tf.truncated_normal_initializer(stddev=0.1)
+bias_initializer = tf.constant_initializer(0.1)
+def variable(name, shape, initializer, dtype=tf.float32, regularizer=None, trainable=True):
+    with tf.device('/cpu:0'):
+        return tf.get_variable(name, shape=shape, dtype=dtype,
+                               initializer=initializer, regularizer=regularizer,
+                               trainable=trainable)
+
+def alex_l2_regularizer(weight=0.0005, scope=None):
+  """Define a L2 regularizer.
+
+  Args:
+    weight: scale the loss by this factor.
+    scope: Optional scope for name_scope.
+
+  Returns:
+    a regularizer function.
+  """
+  def regularizer(tensor):
+    with tf.name_scope(scope, 'L2Regularizer', [tensor]):
+      l2_weight = tf.convert_to_tensor(weight,
+                                       dtype=tensor.dtype.base_dtype,
+                                       name='weight')
+      return tf.multiply(l2_weight, tf.nn.l2_loss(tensor), name='value')
+  return regularizer
+
+#def xwplusb(x, w, b) :
+
+def relu(input):
+      ts = tf.Variable(input, name="W")      
+      ret = tf.nn.relu(ts)
+      return ret
+    
+def conv(input, kernel, biases, k_h, k_w, c_o, s_h, s_w,  padding="VALID", group=1):
+    '''From https://github.com/ethereon/caffe-tensorflow
+    '''
+    c_i = input.get_shape()[-1]
+    assert c_i % group == 0
+    assert c_o % group == 0
+    convolve = lambda i, k: tf.nn.conv2d(i, k, [1, s_h, s_w, 1], padding=padding)
+
+    if group == 1:
+        conv = convolve(input, kernel)
+    else:
+        input_groups = tf.split(input, group, 3)
+        kernel_groups = tf.split(kernel, group, 3)
+        #input_groups = tf.split(3, group, input)
+        #kernel_groups = tf.split(3, group, kernel)
+        output_groups = [convolve(i, k) for i, k in zip(input_groups, kernel_groups)]
+        #conv = tf.concat(3, output_groups)
+        conv = tf.concat(output_groups, 3)
+    return tf.reshape(tf.nn.bias_add(conv, biases), [-1]+conv.get_shape().as_list()[1:])
+
+#def get6W() :
+#    with tf.variable_scope('alexnet_v1',reuse=True) as sc:
+#        l2_regularizer = alex_l2_regularizer(0.0005)                
+#        fc6W = variable('fc6_w', [9216, 4096], initializer=weights_initializer, regularizer=l2_regularizer)
+#        return fc6W
+def softmax(inputs):
+    x = tf.Variable(inputs)    
+    ret = tf.nn.softmax(inputs)
+    return ret;
+def alexnet_v1(inputs, num_classes, is_training=True, weight_decay=0.0005, scope=None):
+    with tf.variable_scope(scope, 'alexnet_v1', [inputs]) as sc:
+        l2_regularizer = alex_l2_regularizer(weight_decay)
+        conv1W = variable('conv1_w', [11, 11, 3, 96], initializer=weights_initializer, regularizer=l2_regularizer)
+        conv1b = variable('conv1_b', [96], initializer=bias_initializer)
+        # Conv1 - Relu - Lrn - Maxpool
+        conv1 = tf.nn.relu(conv(inputs, conv1W, conv1b, 11, 11, 96, 4, 4, padding="SAME", group=1))
+        lrn1 = tf.nn.local_response_normalization(conv1,
+                                                  depth_radius=2,
+                                                  alpha=2e-05,
+                                                  beta=0.75,
+                                                  bias=1.0)
+        maxpool1 = tf.nn.max_pool(lrn1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
+        # Conv2 - Relu - Lrn - Maxpool
+        conv2W = variable('conv2_w', [5, 5, 48, 256], initializer=weights_initializer, regularizer=l2_regularizer)
+        conv2b = variable('conv2_b', [256], initializer=bias_initializer)
+        conv2 = tf.nn.relu(conv(maxpool1, conv2W, conv2b, 5, 5, 256, 1, 1, padding="SAME", group=2))
+        lrn2 = tf.nn.local_response_normalization(conv2,
+                                                  depth_radius=2,
+                                                  alpha=2e-05,
+                                                  beta=0.75,
+                                                  bias=1.0)
+        maxpool2 = tf.nn.max_pool(lrn2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
+        # Conv3 - Conv4 - Conv5 - Maxpool5 - Fc6 -Fc7
+        conv3W = variable('conv3_w', [3, 3, 256, 384], initializer=weights_initializer, regularizer=l2_regularizer)
+        conv3b = variable('conv3_b', [384], initializer=bias_initializer)
+        conv3 = tf.nn.relu(conv(maxpool2, conv3W, conv3b, 3, 3, 384, 1, 1, padding="SAME", group=1))
+        conv4W = variable('conv4_w', [3, 3, 192, 384], initializer=weights_initializer, regularizer=l2_regularizer)
+        conv4b = variable('conv4_b', [384], initializer=bias_initializer)
+        conv4 = tf.nn.relu(conv(conv3, conv4W, conv4b, 3, 3, 384, 1, 1, padding="SAME", group=2))
+        conv5W = variable('conv5_w', [3, 3, 192, 256], initializer=weights_initializer, regularizer=l2_regularizer)
+        conv5b = variable('conv5_b', [256], initializer=bias_initializer)
+        conv5 = tf.nn.relu(conv(conv4, conv5W, conv5b, 3, 3, 256, 1, 1, padding="SAME", group=2))
+        maxpool5 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
+        fc6x = tf.reshape(maxpool5, [-1, int(prod(maxpool5.get_shape()[1:]))])
+#        fc6W = variable('fc6_w', [9216, 4096], initializer=weights_initializer, regularizer=l2_regularizer)
+#        sc.reuse_variable()
+                
+        return fc6x, dict()
+#        fc6W = variable('fc6_w', [9216, 4096], initializer=weights_initializer, regularizer=l2_regularizer)
+#        fc6b = variable('fc6_b', [4096], initializer=bias_initializer)
+#        fc6 = tf.nn.relu_layer(tf.reshape(maxpool5, [-1, int(prod(maxpool5.get_shape()[1:]))]), fc6W, fc6b)
+#        fc7W = variable('fc7_w', [4096, 4096], initializer=weights_initializer, regularizer=l2_regularizer)
+#        fc7b = variable('fc7_b', [4096], initializer=bias_initializer)
+#        fc7 = tf.nn.relu_layer(fc6, fc7W, fc7b)
+#        fc8W = variable('fc8_w', [4096, 1000], initializer=weights_initializer, regularizer=l2_regularizer)
+#        fc8b = variable('fc8_b', [1000], initializer=bias_initializer)
+#        fc8 = tf.nn.xw_plus_b(fc7, fc8W, fc8b)
+#        return fc6W, fc6b, tf.reshape(maxpool5, [-1, int(prod(maxpool5.get_shape()[1:]))]) , dict()#, dict()#,tf.reshape(maxpool5, [-1, int(prod(maxpool5.get_shape()[1:]))]),fc6W, fc6b
+#        prob = tf.nn.softmax(fc8)
+#        print('prob: %s' % prob)
+#        return prob,dict(), tf.reshape(maxpool5, [-1, int(prod(maxpool5.get_shape()[1:]))])
+
---
+++ alexnet/classify.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import sys
+import math
+import json
+import logging
+from datetime import datetime
+import time
+
+import StringIO
+import cStringIO
+from PIL import Image
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.client import timeline
+
+import alexnet_v1
+slim = tf.contrib.slim
+
+class AlexNet(object):
+
+    def __init__(self, ckpt_path, num_classes=1000, label_map_file='label.map', gpu_id=0):
+        self.logger = logging.getLogger(__name__)
+        self.pred_op = None
+        self.sess = None
+        self.net_data=None
+        self.checkpoint_path = ckpt_path
+        self.gpu_id = gpu_id
+        if label_map_file:
+            self.human_labels = self.read_human_readable_label(label_map_file)
+
+        self._build_graph(self.gpu_id)
+
+    def read_human_readable_label(self, label_map_file):
+        human_labels = map(lambda line: line.strip(), open(label_map_file))
+        human_labels = dict(map(lambda i: (i, human_labels[i]), xrange(len(human_labels))))
+        return human_labels
+
+    def my_softmax(self,x):
+        """Compute softmax values for each sets of scores in x."""
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum()
+    
+    def _build_graph(self, gpu_id):
+        """Build the inception graph
+        Args:
+            gpu_id: indicate which gpu is to use
+        """
+        with tf.Graph().as_default():
+            with tf.device('/cpu:0'):
+                inputs = tf.placeholder(tf.float32,
+                                        [None, 224, 224, 3],
+                                        name='input')
+                self.pred_op, _ = alexnet_v1.alexnet_v1(inputs, num_classes=1000, is_training=False)
+#                self.w_op = alexnet_v1.get6W()
+            # Create a session and restore weights
+            gpu_options = tf.GPUOptions(allow_growth=True)
+            sess_config = tf.ConfigProto(
+                gpu_options=gpu_options,
+                allow_soft_placement=True,
+#                device_count={"CPU": 4},
+#                gpu_options=gpu_options,
+                intra_op_parallelism_threads=4,
+                inter_op_parallelism_threads=4               
+            )
+            self.sess = tf.Session(config=sess_config)
+
+            self.logger.info("restore checkpoint file")
+            restore_vars = tf.trainable_variables()
+            self.restore_alexnet_from_npy(self.checkpoint_path, restore_vars)
+            self.logger.info('{}: checkpoint file restored from {}'.format(datetime.now(),
+                                                                              self.checkpoint_path))
+
+    def restore_alexnet_from_npy(self, file_name, variables):
+        alexnet_map = {
+            "alexnet_v1/conv1_w:0": ["conv1", 0],
+            "alexnet_v1/conv1_b:0": ["conv1", 1],
+            "alexnet_v1/conv2_w:0": ["conv2", 0],
+            "alexnet_v1/conv2_b:0": ["conv2", 1],
+            "alexnet_v1/conv3_w:0": ["conv3", 0],
+            "alexnet_v1/conv3_b:0": ["conv3", 1],
+            "alexnet_v1/conv4_w:0": ["conv4", 0],
+            "alexnet_v1/conv4_b:0": ["conv4", 1],
+            "alexnet_v1/conv5_w:0": ["conv5", 0],
+            "alexnet_v1/conv5_b:0": ["conv5", 1],
+            "alexnet_v1/fc6_w:0": ["fc6", 0],
+            "alexnet_v1/fc6_b:0": ["fc6", 1],
+            "alexnet_v1/fc7_w:0": ["fc7", 0],
+            "alexnet_v1/fc7_b:0": ["fc7", 1],
+            "alexnet_v1/fc8_w:0": ["fc8", 0],
+            "alexnet_v1/fc8_b:0": ["fc8", 1]
+        }
+        self.net_data = np.load(file_name).item()
+
+        for variable in variables:
+            if variable.name in alexnet_map.keys():
+                key, index = alexnet_map[variable.name][0], alexnet_map[variable.name][1]
+                self.sess.run(variable.assign(self.net_data[key][index]))
+
+    def preprocess(self, image_buffer):
+        im = Image.open(cStringIO.StringIO(image_buffer)).convert('RGB').resize((224, 224))
+        im = np.array(im).astype(np.float32)
+        im = im - np.mean(im)
+        im[:, :, 0], im[:, :, 2] = im[:, :, 2], im[:, :, 0]
+        return im
+
+    def classify(self, image_data, top_k=1):
+        """Run the graph for classification,
+        Args:
+            1. image_data: one raw image data or batch of preprocessed image data
+            3. top_k: return top k classifications
+        Returns:
+            a top k or a list of top k classifications of format [{'name': cat, 'weight': 0.2}, {'name': 'animal', 'weight': xx}]
+        """
+        if not isinstance(image_data, list):
+            image_data = [image_data]
+        images = []
+        for image in image_data:
+            images.append(self.preprocess(image))
+        start_time = time.time()
+#        run_metadata = tf.RunMetadata()
+        fc6x = self.sess.run(self.pred_op, {'input:0': images},
+ #                                   options=tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),
+ #                                   run_metadata=run_metadata
+        )
+        print('cov time: %s' % (time.time() - start_time))
+        fc6 = np.dot(fc6x, self.net_data['fc6'][0])+ self.net_data['fc6'][1]
+        print('matmul6 time: %s' % (time.time() - start_time))
+        fc7x = np.maximum(fc6,0)
+        print('relu6 time: %s' % (time.time() - start_time))
+        fc7 = np.dot(fc7x, self.net_data['fc7'][0])+ self.net_data['fc7'][1]
+        print('matmul7 time: %s' % (time.time() - start_time))
+        fc8x = np.maximum(fc7,0)
+        print('relu7 time: %s' % (time.time() - start_time))
+        fc8 = np.dot(fc8x, self.net_data['fc8'][0])+ self.net_data['fc8'][1]
+        print('matmul8 time: %s' % (time.time() - start_time))
+  #      softmaxop = alexnet_v1.softmax(fc8)
+ #       newsession = tf.Session()
+#        predictions =newsession.run(softmaxop)
+        predictions = self.my_softmax(fc8)
+        print('softmax time: %s' % (time.time() - start_time))
+#        print(predictions)
+        
+#        fetched_timeline=timeline.Timeline(run_metadata.step_stats)
+#        chrome_trace = fetched_timeline.generate_chrome_trace_format(show_memory=True)
+#        with open('timeline_01.json', 'w') as f:
+#            f.write(chrome_trace)
+
+#        return 1
+        return map(lambda pred: self.get_top_k_classes(pred, top_k), predictions)
+
+    def get_top_k_classes(self, prediction, k):
+        """given the predictions returned by classify, you can get the top k classes
+        that this image may belong to """
+        index = prediction.argsort()[-k:][::-1]
+        classes = [{"name": self.human_labels.get(i, "unknown"), "weight": float(prediction[i])} for i in index if prediction[i] > 0.0]
+        return classes
+
+
+if __name__ == '__main__':
+    prepare_time = time.time()
+    logging.basicConfig(level='INFO', format='%(levelname)s:%(message)s')
+    model = AlexNet('./models/bvlc_alexnet.npy', label_map_file='data/label.map', gpu_id=0)
+    file_name = 'data/cat.jpg'
+    image_buffer = open(file_name).read()
+    print('prepare time: %s' % (time.time() - prepare_time))
+    start_time = time.time()
+    model.classify(image_buffer, top_k=10)
+#    print('classification result: %s' % model.classify(image_buffer, top_k=1))
+    print('spend time: %s' % (time.time() - start_time))
+    sigma=0
+    for i in range(1,100):
+        start_time = time.time()
+ #       model.classify(image_buffer, top_k=10)
+        print('classification result: %s' % model.classify(image_buffer, top_k=10))
+        print('spend time: %s' % (time.time() - start_time))
+        sigma = sigma + time.time() - start_time
+    sigma = sigma/100
+    print(sigma)
+
+    #start_time = time.time()
+    #print('classification result: %s' % model.classify(image_buffer, top_k=1))
+    #print('spend time: %s' % (time.time() - start_time))
+
+    #start_time = time.time()
+    #print('classification result: %s' % model.classify(image_buffer, top_k=1))
+    #print('spend time: %s' % (time.time() - start_time))
+
+
+
